b7ef46d8774c96545bf63b132eded7090c1623cf
Riadz <dzriaddz@gmail.com>
2020-07-26 13:31:40 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// carousel
+$('.owl-carousel').owlCarousel({
+  items: 1,
+  dots: true,
+  nav: true,
+  smartSpeed: 450,
+  margin: 300,
+  autoplay: true,
+  autoplayTimeout: 10000,
+  autoplayHoverPause: true,
+
+  responsive: {
+  }
+});
